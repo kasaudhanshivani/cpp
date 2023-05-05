386ee2e87eb3211ef056bc78aa66166575c2906b
kasaudhanshivani <132655458+kasaudhanshivani@users.noreply.github.com>
2023-05-05 16:00:03 +0530

    Create 07. check whether a number is even or odd.cpp

---
+++ 07. check whether a number is even or odd.cpp
@@ -0,0 +1,16 @@
+
+	
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+	if( n%2==0) {
+		cout <<" print even number";
+	
+		
+	}
+	else {
+		cout<<"  print odd number ";
+	}
+}
