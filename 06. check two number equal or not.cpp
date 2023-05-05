4caf923461c65002b18e5ffb69c4a79fa4649422
kasaudhanshivani <132655458+kasaudhanshivani@users.noreply.github.com>
2023-05-05 15:48:29 +0530

    Create 06. check two number equal or not.cpp

---
+++ 06. check two number equal or not.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x,y;
+	cin>>x >>y ;
+
+	if(x==y){ 
+		cout<<" x and y are  equal " ;
+	
+	}
+
+	else  { 
+		cout<<" x and y  not equal  ";
+	}
+}
