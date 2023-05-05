311f0d201b47d1be376eb1d0d64f93c2c54f9b8d
kasaudhanshivani <132655458+kasaudhanshivani@users.noreply.github.com>
2023-05-05 16:41:16 +0530

    Create 10.grade .cpp

---
+++ 10.grade .cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int a,b,c,d ,e,n;
+	cin>>a>>b>>c>>d>>e;
+	if(n<90&&n<=100   ) { 
+		cout<<" grade A";
+		
+}
+else if ( n>80&&n>90) {
+	cout<<"  grade B";
+}
+else if ( n>60&& n>80) {
+	cout<<" grade c";
+}
+else if( n<60  ) { 
+	cout<<"grade D ";
+}
+}
