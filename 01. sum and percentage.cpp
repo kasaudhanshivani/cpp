3f7b343d558843c3be9f234981ceea545bb76379
kasaudhanshivani <132655458+kasaudhanshivani@users.noreply.github.com>
2023-05-05 14:18:28 +0530

    Create 01. sum and percentage.cpp

---
+++ 01. sum and percentage.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+	float  m,p,c,e,h,sum,percentage;
+	cin>>m>>p>>c>>e>>h>>sum>>percentage;
+	
+	sum=m+p+c+e+h;
+	percentage=sum/5;
+	cout<<sum<<"  "<< percentage<<" "<<endl;
+	
+
+
+
+
+}
