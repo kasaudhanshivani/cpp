9ddbf319b0ab0e7ea92918f365af71842239d690
kasaudhanshivani <132655458+kasaudhanshivani@users.noreply.github.com>
2023-05-05 14:49:44 +0530

    Create 03. area and circumference.cpp

---
+++ 03. area and circumference.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+	float pi=3.14, area,circumference,r;
+	cin>>r;
+	area=pi*r*r;
+	circumference=2*pi*r;
+	cout<<area<<"   "<<circumference <<endl;
+	
+	
+
+
+
+
+}
