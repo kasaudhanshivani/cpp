0896e5b8a68c4caf8cc8262df5355041953186e0
kasaudhanshivani <132655458+kasaudhanshivani@users.noreply.github.com>
2023-05-05 16:19:24 +0530

    Create 09. greatest of three number.cpp

---
+++ 09. greatest of three number.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x,y,n;
+	cin>>n>>y>>x;
+	if (x<y  ) { 
+		cout<<" x is greater than y   ";
+}
+else if (   y<n) {
+	cout<<"  y is greater thnn n";
+}
+else {  
+	cout<<" n is grater than x,y";
+}
+}
