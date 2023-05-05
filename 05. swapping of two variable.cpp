10b5f20f25773678003059ce96faa305da282e85
kasaudhanshivani <132655458+kasaudhanshivani@users.noreply.github.com>
2023-05-05 15:26:47 +0530

    Create 05. swapping of two variable.cpp

---
+++ 05. swapping of two variable.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x,y,temp;
+	
+	cin>>x>>y>>temp;
+	//" swapping of third variable";
+     temp=x;
+	 x=y;
+	y=temp;
+	cout<<x<< " " <<y;
+}
