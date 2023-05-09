d89aa3bc171683560f972031d0ca4487737a40b3
kasaudhanshivani <132655458+kasaudhanshivani@users.noreply.github.com>
2023-05-09 13:40:45 +0530

    Update 10.grade .cpp

--- 10.grade .cpp
+++ 10.grade .cpp
@@ -3,17 +3,17 @@ using namespace std;
 int main() {
 	int a,b,c,d ,e,n;
 	cin>>a>>b>>c>>d>>e;
-	if(n<90&&n<=100   ) { 
+	if(n<=90&&n>=100   ) { 
 		cout<<" grade A";
 		
 }
-else if ( n>80&&n>90) {
+else if ( n<=80&&n>90) {
 	cout<<"  grade B";
 }
-else if ( n>60&& n>80) {
+else if ( n<=60&& n>80) {
 	cout<<" grade c";
 }
-else if( n<60  ) { 
+else if( n<=60  ) { 
 	cout<<"grade D ";
 }
 }
